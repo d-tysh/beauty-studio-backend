f115ddb12d95a7d19f6d052cd813ee756692f5b8
d-tysh <39271488+d-tysh@users.noreply.github.com>
2025-07-27 13:29:44 +0300

    Update Procedure schema

--- service/models/procedure.js
+++ service/models/procedure.js
@@ -3,6 +3,11 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const ProcedureSchema = new Schema({
+    admin: {
+        adminId: Schema.Types.ObjectId,
+        ref: 'admin',
+        required: [true, 'Admin reference is required']
+    },
     client: {
         clientId: Schema.Types.ObjectId,
         ref: 'client',
