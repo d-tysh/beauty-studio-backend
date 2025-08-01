4267cffe089b3c23b760ff09aef96b430ff7f5fb
d-tysh <39271488+d-tysh@users.noreply.github.com>
2025-08-01 13:12:00 +0300

    Update Admin schema

--- service/models/admin.js
+++ service/models/admin.js
@@ -2,34 +2,37 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const AdminSchema = new Schema({
-    name: {
-        type: String,
-        required: [true, 'Name is required']
+const AdminSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Name is required']
+        },
+        login: {
+            type: String,
+            required: [true, 'Login is required'],
+            unique: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            default: ''
+        },
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        status: {
+            type: String,
+            enum: ['basic', 'pro'],
+            default: 'basic'
+        },
+        token: {
+            type: String,
+            default: ''
+        }
     },
-    login: {
-        type: String,
-        required: [true, 'Login is required'],
-        unique: true
-    },
-    email: {
-        type: String,
-        unique: true,
-        default: ''
-    },
-    password: {
-        type: String,
-        required: [true, 'Password is required']
-    },
-    status: {
-        type: String,
-        enum: ['basic', 'pro'],
-        default: 'basic'
-    },
-    token: {
-        type: String,
-        default: ''
-    }
-})
+    { versionKey: false }
+)
 
 export const Admin = mongoose.model('admin', AdminSchema);
