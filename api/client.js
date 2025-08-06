ae7f0dc7d4f5b430e717e4f1968686f05a69c9c2
d-tysh <39271488+d-tysh@users.noreply.github.com>
2025-08-06 16:41:27 +0300

    Update Client API routes

--- api/client.js
+++ api/client.js
@@ -4,11 +4,13 @@ import authenticate from '../middlewares/authenticate.js';
 
 const router = express.Router();
 
+router.use(authenticate);
+
 router
-    .post('/', authenticate, clientCtrl.register)
-    .get('/', authenticate, clientCtrl.getAllClients)
-    .get('/:id', authenticate, clientCtrl.getClientById)
-    .patch('/:id', authenticate, clientCtrl.update)
-    .delete('/:id', authenticate, clientCtrl.remove);
+    .post('/', clientCtrl.register)
+    .get('/', clientCtrl.getAllClients)
+    .get('/:id', clientCtrl.getClientById)
+    .patch('/:id', clientCtrl.update)
+    .delete('/:id', clientCtrl.remove);
 
 export default router;
