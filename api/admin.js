b19286325be2768060f81a5fd5a969a71515792d
d-tysh <39271488+d-tysh@users.noreply.github.com>
2025-08-19 11:29:14 +0300

    Add data validation to Admin register route

--- api/admin.js
+++ api/admin.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import adminCtrl from '../controllers/admin.js';
 import authenticate from '../middlewares/authenticate.js';
+import validateBody from '../middlewares/validateBody.js';
+import { adminRegisterSchema } from '../service/models/admin.js';
 
 const router = express.Router();
 
 router
-    .post('/register', adminCtrl.register)
+    .post('/register', validateBody(adminRegisterSchema), adminCtrl.register)
     .post('/login', adminCtrl.login)
     .post('/logout', adminCtrl.logout)
     .get('/current', authenticate, adminCtrl.getCurrentAdmin)
