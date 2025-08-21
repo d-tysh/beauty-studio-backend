d296924678713dcf6e2d60a98f5679e528e9b16f
d-tysh <39271488+d-tysh@users.noreply.github.com>
2025-08-21 11:12:31 +0300

    Add data validation to Service routes

--- api/service.js
+++ api/service.js
@@ -1,16 +1,18 @@
 import express from 'express';
 import serviceCtrl from '../controllers/service.js';
 import authenticate from '../middlewares/authenticate.js';
+import validateBody from '../middlewares/validateBody.js';
+import { serviceAddSchema, serviceUpdateSchema } from '../service/models/service.js';
 
 const router = express.Router();
 
 router.use(authenticate);
 
 router
-    .post('/', serviceCtrl.add)
+    .post('/', validateBody(serviceAddSchema), serviceCtrl.add)
     .get('/', serviceCtrl.getAllServices)
     .get('/:id', serviceCtrl.getServiceById)
-    .patch('/:id', serviceCtrl.update)
+    .patch('/:id', validateBody(serviceUpdateSchema), serviceCtrl.update)
     .delete('/:id', serviceCtrl.remove);
 
 export default router;
