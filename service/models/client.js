dd4ff23977e14794daa1e1f1e67315a3b11f6dbb
d-tysh <39271488+d-tysh@users.noreply.github.com>
2025-10-06 17:02:32 +0300

    Update Client scheme and controllers

--- service/models/client.js
+++ service/models/client.js
@@ -9,10 +9,6 @@ const ClientSchema = new Schema(
             type: String,
             required: [true, 'Name is required']
         },
-        nameForAdmin: {
-            type: String,
-            default: ''
-        },
         phoneNumber: {
             type: String,
             unique: true,
@@ -32,7 +28,6 @@ const ClientSchema = new Schema(
 
 export const clientRegisterSchema = Joi.object({
     name: Joi.string().min(2).max(50).required(),
-    nameForAdmin: Joi.string().min(2).max(50).allow(''),
     phoneNumber: Joi.string().pattern(/^\+\d{12}$/).required(),
     description: Joi.string().allow(''),
     discount: Joi.number().min(0).max(100)
@@ -40,7 +35,6 @@ export const clientRegisterSchema = Joi.object({
 
 export const clientUpdateSchema = Joi.object({
     name: Joi.string().min(2).max(50),
-    nameForAdmin: Joi.string().min(2).max(50).allow(''),
     phoneNumber: Joi.string().pattern(/^\+\d{12}$/),
     description: Joi.string().allow(''),
     discount: Joi.number().min(0).max(100)
